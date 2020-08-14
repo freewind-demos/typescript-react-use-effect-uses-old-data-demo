42ed908ebd4b88d10745b90f9215e06614b25067
Freewind <nowindlee@gmail.com>
2020-08-14 20:54:25 +0800

    typescript-react-use-effect-uses-old-data-demo

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React Use Effect Uses Old Data Demo
+=============================================
+
+在React每个组件中，useEffect中定义的函数如果引用到了外面的state，实际上使用的是在定义这一刻时state的值。
+
+这实际上跟react无关，而是语言本身就是这样的。
+
+例子来源于：
+
+https://overreacted.io/zh-hans/a-complete-guide-to-useeffect/#%E6%AF%8F%E4%B8%80%E6%AC%A1%E6%B8%B2%E6%9F%93%E9%83%BD%E6%9C%89%E5%AE%83%E8%87%AA%E5%B7%B1%E7%9A%84%E4%BA%8B%E4%BB%B6%E5%A4%84%E7%90%86%E5%87%BD%E6%95%B0
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,23 @@
 //import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function Hello() {
-  return <div>
-    <h1>Hello React</h1>
-  </div>
+  const [count, setCount] = useState(0);
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert('You clicked on: ' + count);
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <button onClick={handleAlertClick}>
+        Show alert
+      </button>
+    </div>
+  );
 };
